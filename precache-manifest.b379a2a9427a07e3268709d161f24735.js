5ae33fa3838ac658a5e34c65d191637733d7526d
Danil Kruhlikov <danylo.kruhlikov@nure.ua>
2020-10-05 14:22:31 +0300

    deploy

---
+++ precache-manifest.b379a2a9427a07e3268709d161f24735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3656c97937beb6ead0796e16ba7cc983",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac89bf03f084b1b96d9",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "dc9983e394c3c0bb1e7b",
+    "url": "/static/css/main.133e3d5c.chunk.css"
+  },
+  {
+    "revision": "aac89bf03f084b1b96d9",
+    "url": "/static/js/2.ce386ae8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce386ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9983e394c3c0bb1e7b",
+    "url": "/static/js/main.da24fb49.chunk.js"
+  },
+  {
+    "revision": "a5414f1b3e66525393d6",
+    "url": "/static/js/runtime-main.66c1263f.js"
+  }
+]);
